5d8a9c8ad7d0f9d45e10740bc2492a6d92665b22
Felipe Neves <88124495+lipeneves@users.noreply.github.com>
2021-09-02 18:33:22 -0300

    Aula 1

---
+++ variaveis/main.js
@@ -0,0 +1,13 @@
+var nome = 'Felipe',
+    idade = 26,
+    comida;
+
+comida = 'Strogonoff';
+
+var time, 
+    corFavorita, 
+    profissao, 
+    altura, 
+    peso;
+
+console.log(nome, idade, comida);
