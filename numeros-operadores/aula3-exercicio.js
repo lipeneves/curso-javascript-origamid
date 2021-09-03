12e366ca38fa67997bef924f9718afc16d7f6bb9
Felipe Neves <88124495+lipeneves@users.noreply.github.com>
2021-09-02 22:41:24 -0300

    Aula 3 - Exercícios

---
+++ numeros-operadores/aula3-exercicio.js
@@ -0,0 +1,21 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; //35
+
+// Crie duas expressões que retornem NaN
+console.log('110,50' * 10); //NaN
+console.log('Total: 50' / 20); //NaN
+
+// Somar a string '200' com o número 50 e retornar 250
+console.log(+'200' + 50);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5;
+x++;
+console.log(x);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);
--- numeros-operadores/aula3.js
+++ numeros-operadores/aula3.js
@@ -1,27 +1,44 @@
-//Operadores Aritméticos
-var x = 2, y = 4;
+// //Operadores Aritméticos
+// var x = 2, y = 4;
 
-var soma = x + y;
-console.log(soma);
+// var soma = x + y;
+// console.log(soma);
 
-var multiplicacao = x * y;
-console.log(multiplicacao);
+// var multiplicacao = x * y;
+// console.log(multiplicacao);
 
-var divisao = y / x;
-console.log(divisao);
+// var divisao = y / x;
+// console.log(divisao);
 
-var subtracao = y - x;
-console.log(subtracao);
+// var subtracao = y - x;
+// console.log(subtracao);
 
-var expoente = x ** y;
-console.log(expoente);
+// var expoente = x ** y;
+// console.log(expoente);
 
-var modulo = 4 % 2; 
-console.log(modulo);
+// var modulo = 4 % 2; 
+// console.log(modulo);
 
-//Testando operações entre strings e números
-var resposta = '100' / 2;
-console.log(typeof resposta);
+// //Testando operações entre strings e números
+// var resposta = '100' / 2;
+// console.log(typeof resposta);
 
-//Testando isNaN
-console.log(isNaN('100'));
+// //Testando isNaN
+// console.log(isNaN('100'));
+
+//Testando ordem das operações
+var calc = 5 + 5 * 2 + 5 / 2;
+console.log(calc);
+
+// Operadores Unários
+var x = 5, y = 5;
+console.log(x++);
+console.log(x);
+
+console.log(++y);
+
+//Unário para conversão
+var num = '-t12';
+console.log(typeof num);
+console.log(-num);
+
