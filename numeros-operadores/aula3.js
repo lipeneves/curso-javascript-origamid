bb84fde3cecffd77dba3e98e7cf90b4b03fce4f6
Felipe Neves <88124495+lipeneves@users.noreply.github.com>
2021-09-02 21:16:18 -0300

    Aula 3 - Números e Operadores

---
+++ numeros-operadores/aula3.js
@@ -0,0 +1,27 @@
+//Operadores Aritméticos
+var x = 2, y = 4;
+
+var soma = x + y;
+console.log(soma);
+
+var multiplicacao = x * y;
+console.log(multiplicacao);
+
+var divisao = y / x;
+console.log(divisao);
+
+var subtracao = y - x;
+console.log(subtracao);
+
+var expoente = x ** y;
+console.log(expoente);
+
+var modulo = 4 % 2; 
+console.log(modulo);
+
+//Testando operações entre strings e números
+var resposta = '100' / 2;
+console.log(typeof resposta);
+
+//Testando isNaN
+console.log(isNaN('100'));
